cb25fd33f45f2cea1838273f57348a8aba6ce0d8
xX-epic-gamer-2007-Xx <139648967+xX-epic-gamer-2007-Xx@users.noreply.github.com>
2023-07-15 17:08:14 -0700

    Update main.c

--- main.c
+++ main.c
@@ -8,14 +8,14 @@ void my_strcpy(char *dest, char *src) {
     char *p = src;
     char *q = dest;
     while (*p) {
-	*q++ = *p++;
+        *q++ = *p++;
     }
     *q = 0;
 
     void *new_ret = __builtin_return_address(0);
     if (new_ret != orig_ret) {
-	fprintf(stderr, "strcpy return address corrupted to %p\n", new_ret);
-	exit(42);
+        fprintf(stderr, "strcpy return address corrupted to %p\n", new_ret);
+        exit(42);
     }
 }
 
@@ -27,16 +27,16 @@ void func(char *attacker_controlled) {
 
     void *new_ret = __builtin_return_address(0);
     if (new_ret != orig_ret) {
-	fprintf(stderr, "func return address corrupted to %p\n", new_ret);
-	exit(42);
+        fprintf(stderr, "func return address corrupted to %p\n", new_ret);
+        exit(42);
     }
 }
 
 int main(int argc, char **argv) {
     if (argc == 2) {
-	func(argv[1]);
+        func(argv[1]);
     } else {
-	func("short");
+        func("short");
     }
     return 0;
 }
